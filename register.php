5cad401980e31430e923c88edf4e697a93c792cd
root <root@g3w2-demo.(none)>
2018-10-31 22:56:45 -0300

    agregando estilos al formulario

--- register.php
+++ register.php
@@ -2,10 +2,51 @@
 <html>
 <head>
 	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 	<div class="header">
 		<h2>Registration</h2>
 	</div>
+
+<form method="post" action="register.php">
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username">
+		</div>
+		
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name">
+		</div>
+
+		<div class="input-group">
+			<label>Last Name</label>
+			<input type="text" name="lastname">
+		</div>
+
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_1">
+		</div>
+
+		<div class="input-group">
+			<button type="submit" name="register" class="button">Register</button>
+		</div>
+		<p>
+			Are you a member? <a href="login.php">Log in</a>
+		</p>
+
+</form>
 </body>
+
+	
+
+
+
 </html>
