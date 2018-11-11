df4a6fde3360edea4afdbf81ff448cdc719db82c
demo <demo@g3w2-demo.(none)>
2018-11-11 17:18:25 -0300

    Modificación de la query

--- index.php
+++ index.php
@@ -40,9 +40,19 @@
 			
 			//$queryTest = "SELECT name FROM users WHERE username = '$username'";
 			
-			$finalQuery = "SELECT user1.username, user2.username FROM users user1,users user2, vistaTemporal vT 
-							WHERE user1.id_u= vT.id1 AND user2.id_u= vT.id2 AND vT.id1 < vT.id2 ORDER BY (user1.username)";
-			
+			//$finalQuery = "SELECT user1.username, user2.username FROM users user1,users user2, vistaTemporal vT 
+			//				WHERE user1.id_u= vT.id1 AND user2.id_u= vT.id2 AND vT.id1 < vT.id2 ORDER BY (user1.username)";
+
+			$finalQuery = "Select u.username, us.username from users u left join users us on u.id_u != us.id_u where u.id_u < us.id_u
+					EXCEPT
+					select distinct u1.username, u2.username from users u1, users u2
+						where exists(select id_pelicula from pelis_que_vio where id_usuario = u1.id_u
+								EXCEPT
+							select id_pelicula from pelis_que_vio where id_usuario = u2.id_u)
+		
+						or exists(select id_pelicula from pelis_que_vio, users as u1 where id_usuario = u2.id_u
+								EXCEPT
+							select id_pelicula from pelis_que_vio where id_usuario = u1.id_u) 	";
 			
 			$result2 = pg_query($finalQuery);
 			echo "<table>\n";
